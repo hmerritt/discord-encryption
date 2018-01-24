0769606a9d7c735cec3f141b20695147aa1cba05
Harry Merritt <Harrymerritt.me@gmail.com>
2018-01-24 22:00:30 +0000

    visual input for encryption password from within discord
    Right-click the lock to change / see the current encryption password

--- encryption.plugin.js
+++ encryption.plugin.js
@@ -16,9 +16,53 @@ class encryption {
     start() {
 
         console.clear();
-
+		
+		//  load password from local storage
+		function load_password() {
+			var localStorageEncryption = localStorage.discordEncryption ? JSON.parse(localStorage.discordEncryption) : {};
+			return localStorageEncryption["password"];
+		}
+		
         //  encryption / decryption password
-        window.shared_password = "CHANGE ME!";
+        window.shared_password = load_password();
+		
+		function encryption_input_toggle() {
+			if ($("#encryptionInput").length == 0) {
+				$('form').append(`
+					<div id="encryptionInput" class="encryptionInput">
+						<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+							<path fill="#ddd" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
+						</svg>
+						<input placeholder="Encryption password" type="password">
+						<script>
+							$("#encryptionInput input").val(shared_password);
+							
+							function encrtption_password_check() {
+								if ($("#encryptionInput input").val().length < 3) {
+									$("#encryptionInput").removeClass("nice-password");
+								} else {
+									$("#encryptionInput").addClass("nice-password");
+								}
+							}
+							encrtption_password_check();
+							$("#encryptionInput input").keyup(function() {
+								encrtption_password_check();
+							});
+
+							$("#encryptionInput svg").click(function() {
+								if ($("#encryptionInput input").attr("type") == "password") {
+									$("#encryptionInput input").attr("type", "text");
+									$("#encryptionInput svg path").attr("d", "M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z");
+								} else {
+									$("#encryptionInput input").attr("type", "password");
+									$("#encryptionInput svg path").attr("d", "M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z");
+								}
+							});
+						</script>
+					</div>
+				`);
+			}
+		}
 		
 		//  make encrypted messages appear green
 		$("head").append(`
@@ -45,6 +89,53 @@ class encryption {
 			.encryptionButton:hover path {
 			  fill: #fff;
 			}
+			#encryptionInput {
+			  position: absolute;
+			  top: -28px;
+			  left: 0px;
+			  width: 280px;
+			  height: 40px;
+			  overflow: hidden;
+			  border-radius: 5px;
+			  background-color: #FF2949;
+			  -webkit-transition: all 280ms ease;
+					  transition: all 280ms ease;
+			}
+
+			#encryptionInput svg {
+			  position: absolute;
+			  cursor: pointer;
+			  top: 8px;
+			  left: 10px;
+			  z-index: 1;
+			}
+
+			#encryptionInput input {
+			  position: absolute;
+			  top: 0px;
+			  left: 34px;
+			  width: 100%;
+			  height: 100%;
+			  border: none;
+			  text-indent: 4px;
+			  border-radius: 5px;
+			  background-color: #FF2949;
+		   	  -webkit-transition: all 280ms ease;
+				      transition: all 280ms ease;
+			  outline: 0px !important;
+			  -webkit-appearance: none;
+			  -webkit-box-shadow: none;
+  			          box-shadow: none;
+			  resize: none;
+			  color: #ddd;
+			  font-size: .88em;
+			  padding: 1px 8px;
+			  white-space: nowrap;
+			}
+			#encryptionInput.nice-password,
+			#encryptionInput.nice-password input {
+			  background-color: rgb(67, 181, 129);
+			}
 		  </style>
 		`);
 
@@ -56,6 +147,12 @@ class encryption {
             addedNodes.length && addedNodes[0].classList && addedNodes[0].classList.contains('message') ||
             addedNodes.length && addedNodes[0].classList && addedNodes[0].classList.contains('hide-overflow') ||
             addedNodes.length && addedNodes[0].classList && addedNodes[0].classList.contains('messages-wrapper')) {
+			
+			//  load password from local storage
+			function load_password() {
+				var localStorageEncryption = localStorage.discordEncryption ? JSON.parse(localStorage.discordEncryption) : {};
+				return localStorageEncryption["password"];
+			}
 
             //  encrypt message using set password - add an encryption prefix for identification
             function msg_enc(msg_original) {
@@ -86,6 +183,62 @@ class encryption {
             }
             decrypt_all_msg();
 			
+			//  toggle input for encryption password change
+			function encryption_input_toggle(toggle) {
+				if (toggle == "show" || toggle == "" && $("#encryptionInput").length == 0) {
+					if ($("#encryptionInput").length == 0) {
+						$('form').append(`
+							<div id="encryptionInput" class="encryptionInput">
+								<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+									<path fill="#ddd" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
+								</svg>
+								<input placeholder="Encryption password" type="password">
+								<script>
+									$("#encryptionInput input").val(shared_password);
+									
+									//  save password to local storage
+									function save_password(password) {
+										var objPassword = {"password": password};
+										localStorage.discordEncryption = JSON.stringify(objPassword);
+										shared_password = password;
+									}
+									//  load password from local storage
+									function load_password() {
+										var localStorageEncryption = localStorage.discordEncryption ? JSON.parse(localStorage.discordEncryption) : {};
+										return localStorageEncryption["password"];
+									}
+						
+									function encrtption_password_check() {
+										if ($("#encryptionInput input").val().length < 3) {
+											$("#encryptionInput").removeClass("nice-password");
+										} else {
+											$("#encryptionInput").addClass("nice-password");
+											save_password($("#encryptionInput input").val());
+										}
+									}
+									encrtption_password_check();
+									$("#encryptionInput input").keyup(function() {
+										encrtption_password_check();
+									});
+
+									$("#encryptionInput svg").click(function() {
+										if ($("#encryptionInput input").attr("type") == "password") {
+												$("#encryptionInput input").attr("type", "text");
+												$("#encryptionInput svg path").attr("d", "M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z");
+										} else {
+											$("#encryptionInput input").attr("type", "password");
+											$("#encryptionInput svg path").attr("d", "M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z");
+										}
+									});
+								</script>
+							</div>
+						`);
+					}
+				} else {
+					$("#encryptionInput").remove();
+				}
+			}
+			
 			//  add encryption button - click to encrypt / decrypt message
 			function add_encryption_button() {
 				if (document.getElementById("encryptionButton") == null || document.getElementById("encryptionButton") == undefined) {
@@ -108,11 +261,20 @@ class encryption {
 							textareaInstance.setState({textValue:msg_enc(msg)});
 						}
 					});
+					
+					if (load_password().length < 3) {
+						encryption_input_toggle("show");
+					}
+					$(".encryptionButton").bind("contextmenu",function(e) {
+						e.preventDefault();
+						encryption_input_toggle("");
+					});
 				}
 			}
 			add_encryption_button();
 			
-			$(".channel, .containerDefault-7RImuF").click(function() {
+			$(".guild, .channel, .containerDefault-7RImuF").click(function() {
+				encryption_input_toggle("hide");
 				setTimeout(function(){
 					add_encryption_button();
 				}, 88);
@@ -130,7 +292,7 @@ class encryption {
     }
 
     getVersion() {
-        return '1.1.1';
+        return '1.2.1';
     }
 
     getDescription() {
