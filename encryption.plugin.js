a06cf89ae6e746060cb71000bc1bba070c513b8a
Harry Merritt <Harrymerritt.me@gmail.com>
2018-01-25 14:14:40 +0000

    visual input for encryption password from within discord

--- encryption.plugin.js
+++ encryption.plugin.js
@@ -17,55 +17,21 @@ class encryption {
 
         console.clear();
 		
+		//  encryption / decryption password
+		window.shared_password = "";
+		
 		//  load password from local storage
 		function load_password() {
 			var localStorageEncryption = localStorage.discordEncryption ? JSON.parse(localStorage.discordEncryption) : {};
 			return localStorageEncryption["password"];
 		}
 		
-        //  encryption / decryption password
-        window.shared_password = load_password();
-		
-		if (shared_password == null || shared_password == undefined) {
-			shared_password = "";
+		try {
+			shared_password = load_password();
+			if(shared_password == null || shared_password == undefined) throw "error";
 		}
-		
-		function encryption_input_toggle() {
-			if ($("#encryptionInput").length == 0) {
-				$('form').append(`
-					<div id="encryptionInput" class="encryptionInput">
-						<svg style="width:24px;height:24px" viewBox="0 0 24 24">
-							<path fill="#ddd" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
-						</svg>
-						<input placeholder="Encryption password" type="password">
-						<script>
-							$("#encryptionInput input").val(shared_password);
-							
-							function encrtption_password_check() {
-								if ($("#encryptionInput input").val().length < 3) {
-									$("#encryptionInput").removeClass("nice-password");
-								} else {
-									$("#encryptionInput").addClass("nice-password");
-								}
-							}
-							encrtption_password_check();
-							$("#encryptionInput input").keyup(function() {
-								encrtption_password_check();
-							});
-
-							$("#encryptionInput svg").click(function() {
-								if ($("#encryptionInput input").attr("type") == "password") {
-									$("#encryptionInput input").attr("type", "text");
-									$("#encryptionInput svg path").attr("d", "M11.83,9L15,12.16C15,12.11 15,12.05 15,12A3,3 0 0,0 12,9C11.94,9 11.89,9 11.83,9M7.53,9.8L9.08,11.35C9.03,11.56 9,11.77 9,12A3,3 0 0,0 12,15C12.22,15 12.44,14.97 12.65,14.92L14.2,16.47C13.53,16.8 12.79,17 12,17A5,5 0 0,1 7,12C7,11.21 7.2,10.47 7.53,9.8M2,4.27L4.28,6.55L4.73,7C3.08,8.3 1.78,10 1,12C2.73,16.39 7,19.5 12,19.5C13.55,19.5 15.03,19.2 16.38,18.66L16.81,19.08L19.73,22L21,20.73L3.27,3M12,7A5,5 0 0,1 17,12C17,12.64 16.87,13.26 16.64,13.82L19.57,16.75C21.07,15.5 22.27,13.86 23,12C21.27,7.61 17,4.5 12,4.5C10.6,4.5 9.26,4.75 8,5.2L10.17,7.35C10.74,7.13 11.35,7 12,7Z");
-								} else {
-									$("#encryptionInput input").attr("type", "password");
-									$("#encryptionInput svg path").attr("d", "M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z");
-								}
-							});
-						</script>
-					</div>
-				`);
-			}
+		catch (error) {
+			shared_password = "";
 		}
 		
 		//  make encrypted messages appear green
@@ -95,15 +61,18 @@ class encryption {
 			}
 			#encryptionInput {
 			  position: absolute;
-			  top: -28px;
+			  top: -10px;
 			  left: 0px;
 			  width: 280px;
 			  height: 40px;
 			  overflow: hidden;
 			  border-radius: 5px;
 			  background-color: #FF2949;
-			  -webkit-transition: all 280ms ease;
-					  transition: all 280ms ease;
+			  -webkit-transition: all 280ms ease 10ms;
+					  transition: all 280ms ease 10ms;
+			}
+			#encryptionInput.open {
+				top: -28px;
 			}
 
 			#encryptionInput svg {
@@ -192,7 +161,7 @@ class encryption {
 				if (toggle == "show" || toggle == "" && $("#encryptionInput").length == 0) {
 					if ($("#encryptionInput").length == 0) {
 						$('form').append(`
-							<div id="encryptionInput" class="encryptionInput">
+							<div id="encryptionInput" class="encryptionInput open">
 								<svg style="width:24px;height:24px" viewBox="0 0 24 24">
 									<path fill="#ddd" d="M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9M12,17A5,5 0 0,1 7,12A5,5 0 0,1 12,7A5,5 0 0,1 17,12A5,5 0 0,1 12,17M12,4.5C7,4.5 2.73,7.61 1,12C2.73,16.39 7,19.5 12,19.5C17,19.5 21.27,16.39 23,12C21.27,7.61 17,4.5 12,4.5Z" />
 								</svg>
@@ -266,7 +235,7 @@ class encryption {
 						}
 					});
 					
-					if (load_password().length < 3) {
+					if (shared_password.length < 3) {
 						encryption_input_toggle("show");
 					}
 					$(".encryptionButton").bind("contextmenu",function(e) {
