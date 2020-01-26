9016d20de2bd50701ee504db6731ebe4463f84b1
Harry Merritt <harry@merritt.es>
2020-01-26 20:25:30 +0000

    Add encryption helper functions - get encryption state - get channel id

--- encryption.plugin.js
+++ encryption.plugin.js
@@ -28,7 +28,7 @@ class encryption {
                         {
                             global: {
                                 password: "",
-                                state:    "off"
+                                state:    false
                             }
                         };
 
@@ -62,7 +62,8 @@ class encryption {
         //  Inject styles
         this.inject('styles', 'head', 'append', this.components.styles);
 
-        //  TODO: Inject lock icon into messages page
+        //  Inject encryption button on start
+        this.components.encryptionButton.inject();
     }
 
 
@@ -76,6 +77,7 @@ class encryption {
 
         //  Unbind event listners
         $(document).off('click', '.da-channel, .da-listItem, .da-containerDefault');
+        $(document).off('click', `[${this.script.name}].updatePanel`);
     }
 
 
@@ -214,6 +216,46 @@ class encryption {
 
 
         /*
+        * Discord
+        * -> helper functions to handle discord things
+        */
+        this.discord = {};
+        this.discord.get = {};
+
+        //  Get the channel id for current chat
+        this.discord.get.channelId = () =>
+        {
+            return window.location.pathname.split('/').pop();
+        }
+
+
+        /*
+        * Encryption
+        * -> helper functions to handle the encrypt/decrypting
+        */
+        this.encryption = {};
+        this.encryption.get = {};
+        this.encryption.set = {};
+
+        /*
+        * Get encryption state for current chat
+        * @return bool
+        */
+        this.encryption.get
+        this.encryption.get.state = () =>
+        {
+            let channelId   = this.discord.get.channelId();
+            let globalState = this.userData.global.state;
+            let chatState   = this.userData[channelId] ? this.userData[channelId].state : false;
+            if (globalState || chatState)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+        /*
         * Encryption button
         * -> displays on every message textbox
         */
@@ -221,7 +263,7 @@ class encryption {
         this.components.encryptionButton.html = () =>
         {
             return `
-                <button ${this.script.name}="encryptionButton" state="off" class="encryptionButton">
+                <button ${this.script.name}="encryptionButton" state="${this.encryption.get.state()}" class="encryptionButton">
                     <svg viewBox="0 0 24 24">
                         <path fill d="M18,8H17V6A5,5 0 0,0 12,1A5,5 0 0,0 7,6V8H6A2,2 0 0,0 4,10V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V10A2,2 0 0,0 18,8M8.9,6C8.9,4.29 10.29,2.9 12,2.9C13.71,2.9 15.1,4.29 15.1,6V8H8.9V6M16,16H13V19H11V16H8V14H11V11H13V14H16V16Z" />
                     </svg>
@@ -242,7 +284,7 @@ class encryption {
 
         /*
         * Toggle the button state
-        * -> changes color
+        * -> changes button color
         */
         this.components.encryptionButton.toggleState = () =>
         {
@@ -260,6 +302,10 @@ class encryption {
             }
         }
 
+        /*
+        * Makes sure button is always injected
+        * Re-inject button when user changes chat/channel
+        */
         $(document).on('click', '.da-channel, .da-listItem, .da-containerDefault', function()
         {
             this.components.encryptionButton.inject();
@@ -291,11 +337,13 @@ class encryption {
             this.fade(`[${this.script.name}].updatePanel`, 'out', delay);
         };
 
-        //  Click
-        //  opens github repo / closes panel
+        /*
+        * Click
+        * opens github repo / closes update panel
+        */
         $(document).on('click', `[${this.script.name}].updatePanel`, (evt) =>
         {
-            // Check if user clicked button or ignoreUpdates is on
+            //  Check if user clicked close button or ignoreUpdates is on
             if (evt['target']['localName'] !== 'span' && !this.script.version.ignoreUpdate)
             {
                 //  Open link to GitHub
@@ -331,17 +379,17 @@ class encryption {
                   height: 24px;
                 }
 
-                .encryptionButton[state=on] path {
+                .encryptionButton[state=true] path {
                   fill: #43b581;
                 }
-                .encryptionButton[state=off] path {
+                .encryptionButton[state=false] path {
                   fill: #B9BBBE;
                 }
 
-                .encryptionButton[state=on]:hover path {
+                .encryptionButton[state=true]:hover path {
                   fill: #1C9C6D;
                 }
-                .encryptionButton[state=off]:hover path {
+                .encryptionButton[state=false]:hover path {
                   fill: #dedede;
                 }
 
