592bd1e13e05e03d20f1207d8d650bfcf25d7626
hmerritt <harry@mrrtt.me>
2023-07-24 23:59:49 +0100

    chore: copy output file to betterDiscord for development

--- build/build-patch.cjs
+++ build/build-patch.cjs
@@ -1,13 +1,20 @@
+const fs = require('fs');
+const os = require('os');
 const pathfs = require('path');
 const prependFile = require('prepend-file');
 
 const pluginOutputFile = pathfs.join(__dirname, '../', 'encryption.plugin.js');
+const betterDiscordPluginDir = `C:/Users/${os.userInfo().username}/AppData/Roaming/BetterDiscord/plugins`;
 
 const main = async () => {
 	console.log(`> Patching build`);
 
 	console.log(`> Adding META data to plugin output file`);
 	await prependFile(pluginOutputFile, `//META{ "name":"encryption", "website":"https://github.com/hmerritt/discord-encryption" }*//\n`);
+
+	// Uncomment when in development
+	// console.log(`> Copy output file to betterDiscord plugin directory`);
+	// fs.copyFileSync(pluginOutputFile, `${betterDiscordPluginDir}/encryption.plugin.js`);
 
 	console.log('> Patching complete :)');
 };
